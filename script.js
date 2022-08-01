999feb7ca3b6f40a0691f47930cbc295c70c8a82
Sunny-kumar-gupta <99629916+Sunny-kumar-gupta@users.noreply.github.com>
2022-08-01 09:24:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//accessing the second paragraph and adding text to the paragraph
+document.getElementById("p1").innerHTML="Hello Javascript" ;
+//accessing the secod paragraph and adding the textcolor into it
+document.getElementById("p1").style.color="red";
+//accessing the first paragraph and modyfing the font size of it
+document.getElementById('intro').style.fontSize='35px';
+//accessing the whole body elements of the Html and adding the background color in to it 
+document.body.style.backgroundColor="lightyellow";
+
+document.body.style.border="5px";
